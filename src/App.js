a77061ceace37f5465e8e2ae4669eede49a9d294
Trusha Bhakta <trushabhakta@Trushas-MacBook-Pro.local>
2023-09-26 00:22:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import TopNav from './Components/TopNav/TopNav';
+import CatNav from './Components/CatNav';
+import MainComponent from './Components/MainComponent';
+import SideNav from './Components/SideNav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <TopNav/>
+      <CatNav/>
+      <MainComponent/>
+      <SideNav/>
+    </div>  
+  )
+};
 
 export default App;
